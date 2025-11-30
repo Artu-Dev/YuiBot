524e9ae6b80cab561a777ff2692fe1b60ca18975
ArturReis <arturreis.dev@gmail.com>
2025-11-30 00:20:55 -0300

    change achievments to emoji

--- functions/image.js
+++ functions/image.js
@@ -2,20 +2,14 @@ import { createCanvas, loadImage } from '@napi-rs/canvas';
 import { GlobalFonts } from "@napi-rs/canvas";
 
 GlobalFonts.registerFromPath("./fonts/Minecraftia.ttf", "Minecraftia");
-
+GlobalFonts.registerFromPath("C:/Windows/Fonts/seguiemj.ttf", "Segoe UI Emoji");
 
 export async function gerar_conquista(usuario, achievement, size = "normal") {
     const template = await loadImage('./img/achievmentTemplate.png');
-    // const template = await loadImage(`https://mcgen.menzerath.eu/api/v1/achievement?background=potion&title=${encodeURI(achievement.name)}&text=${encodeURI(achievement.description)}`);
-
-    
-    
     const canvas = createCanvas(template.width, template.height);
     const ctx = canvas.getContext('2d');
     
     ctx.drawImage(template, 0, 0);
-
-    // return canvas.toBuffer("image/png");
 
     if(size === "small") {
         ctx.font = "17px Minecraftia";
@@ -35,9 +29,9 @@ export async function gerar_conquista(usuario, achievement, size = "normal") {
         ctx.fillText(`${achievement.description}`, 65, 60);
     }
 
-    const avatarURL = usuario.displayAvatarURL({ extension: "png", size: 256 });
-    const avatar = await loadImage(avatarURL);
-    ctx.drawImage(avatar, 12, 12, 40, 40);
-
+    ctx.font = "32px Segoe UI Emoji"
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(achievement.emoji, 12 + 20, 12 + 20 + 5);
     return canvas.toBuffer("image/png");
 }
