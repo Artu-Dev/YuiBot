08d8fcef0b58d9682528c97c092b72f2a6547f6c
ArturReis <arturreis.dev@gmail.com>
2026-04-13 11:59:27 -0300

    overhaul daily command

--- commands/daily.js
+++ commands/daily.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { addChars } from "../database.js";
+import { addChars, getOrCreateUser, setUserProperty, getClassModifier } from "../database.js";
 
 export const name = "dia";
 
@@ -9,26 +9,36 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(client, data) {
     const { userId, guildId, displayName } = data;
+
     const userData = getOrCreateUser(userId, displayName, guildId);
     const lastDaily = userData.lastDailyBonus || 0;
     const now = dayjs();
 
     if (now.diff(dayjs(lastDaily), 'hour') < 24) {
         const nextDaily = dayjs(lastDaily).add(24, 'hour');
-        const timeLeft = nextDaily.diff(now);
+        const diffMs = nextDaily.diff(now);
 
-        const hours = Math.floor(timeLeft / (1000 * 60 * 60));
-        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        const hours = Math.floor(diffMs / (1000 * 60 * 60));
+        const minutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diffMs % (1000 * 60)) / 1000);
+
         const timeString = `${hours}h ${minutes}m ${seconds}s`;
 
-        await data.reply({ content: `Você já resgatou seu prêmio diário. Tente novamente em ${timeString}.`, ephemeral: true });
+        await data.reply({ 
+            content: `Você já resgatou seu prêmio diário. Tente novamente em **${timeString}**.`, 
+            ephemeral: true 
+        });
         return;
-    } else {
-        const classLuckyMod = getClassModifier(userData.user_class, 'lucky') || 0;  
-        const randomChars = Math.floor(Math.random() * 100) + 50 + Math.floor(50 * classLuckyMod); 
-        addChars(userId, guildId, randomChars);
-        setUserProperty('lastDailyBonus', userId, guildId, now.toISOString());
-        await data.reply({ content: `Você resgatou seu prêmio diário de **${randomChars} chars**!.`, ephemeral: true });
     }
+
+    const classLuckyMod = getClassModifier(userData.user_class, 'lucky') || 0;
+    const randomChars = Math.floor(Math.random() * 100) + 50 + Math.floor(50 * classLuckyMod);
+
+    addChars(userId, guildId, randomChars);
+    setUserProperty('lastDailyBonus', userId, guildId, now.toISOString());
+
+    await data.reply({ 
+        content: `Você resgatou seu prêmio diário de **${randomChars} chars**!`, 
+        ephemeral: true 
+    });
 }
