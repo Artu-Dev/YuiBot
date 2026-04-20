ccbe815b620aee48c259fe960c8f81778cc29fbe
ArturReis <arturreis.dev@gmail.com>
2026-04-20 19:52:12 -0300

    randomReply chances adjust

--- functions/handlers/messageCreate/randomActions.js
+++ functions/handlers/messageCreate/randomActions.js
@@ -17,9 +17,9 @@ const AI_COOLDOWN_MS = ms('12s');
 const COOLDOWN_CLEANUP_MS = ms('1m');
 const COOLDOWN_TTL_FACTOR = 5;
 
-const RESEND_CHANCE = 0.01;
+const RESEND_CHANCE = 0.005;
 const MENTION_REPLY_CHANCE = 0.5;
-const RANDOM_REPLY_CHANCE = 0.05;
+const RANDOM_REPLY_CHANCE = 0.01;
 
 const aiCooldowns = new Map();
 let cooldownCleanupInterval = null;
